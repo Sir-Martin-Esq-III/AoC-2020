d1a10add9b181ae203c0232d68e63fa9759b01d7
Thomas Martin <ThomasMartin98@hotmail.com>
2020-12-02 19:57:11 +0000

    Update Day-2.py
    Made it better

--- Day-2.py
+++ Day-2.py
@@ -1,17 +1,14 @@
 def Main():
     f = open("Day-2-Passwords.txt", "r")
-    p1Counter = 0#Result for part 1
-    p2Counter = 0#Result for part 2
+    p1Counter = p2Counter = 0
     for x in f:#
         currentPolicy = x.split()#Split the string in to different parts
         min, max = int(currentPolicy[0].split('-')[0]), int(currentPolicy[0].split('-')[1]) #find the min/max vaules
-        Letter = currentPolicy[1][0]#Find the letter to search for
-        pw = currentPolicy[2] #Find the password
+        Letter,pw = currentPolicy[1][0],currentPolicy[2]#Get the letter and pw
         p1Counter+=PartOne(min,max,Letter,pw)
         p2Counter+=PartTwo(min,max,Letter,pw)
     f.close()
     return(p1Counter,p2Counter)
-
 
 #Checks validation for part 1
 def PartOne(min,max,Letter,pw):
@@ -19,17 +16,10 @@ def PartOne(min,max,Letter,pw):
     for cururentLetter in pw:
         if cururentLetter==Letter:
             instanceCounter+=1
-    if instanceCounter<=max and instanceCounter>=min:
-        return 1
-    else:
-        return 0
+    return int(min<=instanceCounter<=max)
 
 #Checks validation for part 2
 def PartTwo(pos1,pos2,Letter,pw):
-        if (pw[pos1-1]==Letter and pw[pos2-1]!=Letter) or (pw[pos1-1]!=Letter and pw[pos2-1]==Letter):
-            return 1
-        else:
-            return 0
-
+    return int((pw[pos1-1]==Letter) ^ (pw[pos2-1]==Letter))
 
 print(Main())
